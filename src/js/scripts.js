a9e3cd0d52325697d563b9ac617685806201bb12
En Sabah Nur <kelsey.jackson@rockfishinteractive.com>
2017-01-14 11:18:02 -0600

    cooking with gas

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,30 +2,30 @@ $(document).foundation();
 
 $(document).ready(function() {
     $('.photo1-1').one('click', function(){
-        $('<img src="img/photos/large/photo1.jpg" alt="Photo one">').insertAfter('#photo1-1 .modalTitle');
+        $('#photo1-1').append('<h2 class="modalTitle">Photo</h2><img src="img/photos/photo1-large.jpg" alt="Photo one"><div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo1-2').one('click', function(){
-        $('<img src="img/photos/large/photo2.jpg" alt="Photo two">').insertAfter('#photo1-2 .modalTitle');
+        $('#photo1-2').append('<h2 class="modalTitle">Photo</h2><img src="img/photos/photo2-large.jpg" alt="Photo two"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo1-3').one('click', function(){
-        $('<img src="img/photos/large/photo3.jpg" alt="Photo three">').insertAfter('#photo1-3 .modalTitle');
+        $('#photo1-3').append('<h2 class="modalTitle">Photo</h2> <img src="img/photos/photo3-large.jpg" alt="Photo three"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo2-1').one('click', function(){
-        $('<img src="img/photos/large/photo4.jpg" alt="Photo four">').insertAfter('#photo2-1 .modalTitle');
+        $('#photo2-1').append('<h2 class="modalTitle">Photo</h2> <img src="img/photos/photo4-large.jpg" alt="Photo four"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo2-2').one('click', function(){
-        $('<img src="img/photos/large/photo5.jpg" alt="Photo five">').insertAfter('#photo2-2 .modalTitle');
+        $('#photo2-2').append('<h2 class="modalTitle">Photo</h2> <img src="img/photos/photo5-large.jpg" alt="Photo five"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo2-3').one('click', function(){
-        $('<img src="img/photos/large/photo6.jpg" alt="Photo six">').insertAfter('#photo2-3 .modalTitle');
+        $('#photo2-3').append('<h2 class="modalTitle">Photo</h2> <img src="img/photos/photo6-large.jpg" alt="Photo six"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo3-1').one('click', function(){
-        $('<img src="img/photos/large/photo7.jpg" alt="Photo seven">').insertAfter('#photo3-1 .modalTitle');
+        $('#photo3-1').append('<h2 class="modalTitle">Photo</h2> <img src="img/photos/photo7-large.jpg" alt="Photo seven"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo3-2').one('click', function(){
-        $('<img src="img/photos/large/photo8.jpg" alt="Photo eight">').insertAfter('#photo3-2 .modalTitle');
+        $('#photo3-2').append('<h2 class="modalTitle">Photo</h2> <img src="img/photos/photo8-large.jpg" alt="Photo eight"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
     $('.photo3-3').one('click', function(){
-        $('<img src="img/photos/large/photo9.jpg" alt="Photo nine" >').insertAfter('#photo3-3 .modalTitle');
+        $('#photo3-3').append('<h2 class="modalTitle">Photo</h2> <img src="img/photos/photo9-large.jpg" alt="Photo nine"> <div class="info"> <div class="avatar thumb-7"></div> <span class="attribution">Photo by <b>Werner Bechtelar</b></span> </div> <a class="close-reveal-modal" aria-label="Close">&#215;</a>');
     });
 });
